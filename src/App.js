2b25e16e7069466668dd72e79cb895b08a2aaaba
Martín Fiori <martinfiori@outlook.com>
2021-11-26 00:16:01 -0300

    nice css

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import './App.css';
 // Components
 import NavBar from './components/NavBar/NavBar.jsx';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
-import Contador from './components/Contador/Contador.jsx'
-
 
 function App() {
   return (
     <>
     <NavBar/>
     <ItemListContainer/>
-    {/* <Contador/> */}
     </>
   );
 }
