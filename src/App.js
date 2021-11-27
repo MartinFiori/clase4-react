483f018014a9602eaf5d11a4c4e7612c49ac7f99
Martín Fiori <martinfiori@outlook.com>
2021-11-26 21:01:25 -0300

    todounrepoparapilar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 // Components
 import NavBar from './components/NavBar/NavBar.jsx';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
+import ItemCount  from './components/ItemCount/ItemCount.jsx';
 
 function App() {
   return (
     <>
-    <NavBar/>
+    <NavBar/> 
     <ItemListContainer/>
+    <ItemCount stock={2}/>
+    <ItemCount stock={6}/>
+    <ItemCount stock={10}/>
     </>
   );
 }
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,50 @@
+import { render } from '@testing-library/react';
+import './ItemCount.css'
+import React, { useState } from 'react';
+
+
+const ItemCount = ({stock})=>{
+    const [count, setCount]= useState(0)
+    let [totalStock, setTotalStock]= useState(stock)
+
+    const handleSumar = ()=>{
+        count < totalStock && setCount( count + 1 )  
+    }
+
+    const handleRestar = ()=>{
+        count >= 1 && setCount( count - 1 )
+    }
+
+    const handleComprar = ()=>{
+            setCount(
+                count-count
+            )
+
+            setTotalStock(
+                totalStock-count
+            )
+    }
+
+    return(
+        <div className="contador__container">
+            <p className="contador__stock">Stock disponible:{totalStock}</p>
+            <div className="contador">
+                <button className="contador__boton" onClick={handleRestar}>
+                    -
+                </button>
+                <span className="contador__cantidad">
+                    {count}
+                </span>
+                <button className="contador__boton" onClick={handleSumar}>
+                    +
+                </button>
+            </div>
+            <button className="contador__comprar" onClick={handleComprar}>
+                Añadir al carrito
+            </button>
+        </div>
+    )
+}
+
+
+export default ItemCount;
